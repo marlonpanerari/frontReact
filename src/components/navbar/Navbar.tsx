6dc4d87696b1949791500231606637db4a49884e
Marlon Panerari <marpan.dev@gmail.com>
2026-05-12 09:04:25 -0300

    feat: add Tailwind CSS setup and application routes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
     return (
         <>
             <div className='w-full flex justify-center py-4
             			   bg-black text-white'>
             
-                <div className="container flex justify-between text-lg mx-8">
-                    Blog Pessoal
+                <div className="container flex justify-between text-l mx-8">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
-                    <div className='flex gap-6'>
+                    <div className='flex gap-4'>
                         Postagens
                         Temas
                         Cadastrar tema
