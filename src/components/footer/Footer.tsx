c140e7608ea2df743f89ed03ea6b38535fc89bba
Marlon Panerari <marpan.dev@gmail.com>
2026-05-12 10:17:49 -0300

    feat(auth): add login and registration

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,9 +10,9 @@ function Footer() {
             <div className="flex justify-center bg-black text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
+                            Blog Pessoal | Copyright: {data}
                         </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <p className='text-lg'>Acesse as redes sociais</p>
                     <div className='flex gap-2'>
                         <a href="https://www.linkedin.com/in/seu_usuario" target="_blank">
                             <LinkedinLogoIcon size={32} weight='bold' />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,26 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout() {
+
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+
     return (
         <>
             <div className='w-full flex justify-center py-4
-            			   bg-black text-white'>
+            			   bg-indigo-900 text-white'>
             
-                <div className="container flex justify-between text-l mx-8">
+                <div className="container flex justify-between text-lg mx-8">
                     <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
@@ -14,7 +28,7 @@ function Navbar() {
                         Temas
                         Cadastrar tema
                         Perfil
-                        Sair
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
             </div>
